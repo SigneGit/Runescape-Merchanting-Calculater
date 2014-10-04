1f8cf9f5f6111ea649047b4718963afd52261524
xzero213 <xzero213@gmail.com>
2014-10-03 20:23:32 -0500

    Merchanting Calculater for Runescape

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Runescape_Merchanting_Calculater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LabelProfit = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.labelMargin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selling For:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Buying For:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Profit:";
+            // 
+            // LabelProfit
+            // 
+            this.LabelProfit.AutoSize = true;
+            this.LabelProfit.Location = new System.Drawing.Point(55, 161);
+            this.LabelProfit.Name = "LabelProfit";
+            this.LabelProfit.Size = new System.Drawing.Size(13, 13);
+            this.LabelProfit.TabIndex = 5;
+            this.LabelProfit.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Quantity:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 138);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Input: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Output: ";
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Location = new System.Drawing.Point(55, 178);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(13, 13);
+            this.labelInput.TabIndex = 10;
+            this.labelInput.Text = "0";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(55, 195);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(13, 13);
+            this.labelOutput.TabIndex = 11;
+            this.labelOutput.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Copy To ClipBoard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 21);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(149, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 5);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Item Name:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 211);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Margin: ";
+            // 
+            // labelMargin
+            // 
+            this.labelMargin.AutoSize = true;
+            this.labelMargin.Location = new System.Drawing.Point(55, 211);
+            this.labelMargin.Name = "labelMargin";
+            this.labelMargin.Size = new System.Drawing.Size(13, 13);
+            this.labelMargin.TabIndex = 16;
+            this.labelMargin.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(173, 254);
+            this.Controls.Add(this.labelMargin);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LabelProfit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RSMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LabelProfit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelMargin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Runescape_Merchanting_Calculater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int BuyingFor = 0;
+
+        public int SellingFor = 0;
+
+        public int Quantity = 0;
+
+        public int Profit = 0;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+        public void Calculate()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+                return;
+            if (string.IsNullOrEmpty(textBox2.Text))
+                return;
+            if (string.IsNullOrEmpty(textBox3.Text))
+                return;
+            try
+            {
+                SellingFor = Convert.ToInt32(textBox1.Text);
+            }
+            catch (OverflowException)
+            {
+                LabelProfit.Text = "Int to large!";
+                textBox1.Text = "";
+                return;
+            }
+            catch (FormatException)
+            {
+                label1.Text = "Letters cannot be entered";
+                textBox1.Text = "";
+                return;
+            }
+            try
+            {
+                BuyingFor = Convert.ToInt32(textBox2.Text);
+            }
+            catch (OverflowException)
+            {
+                LabelProfit.Text = "Int to large!";
+                textBox2.Text = "";
+                return;
+            }
+            catch (FormatException)
+            {
+                label2.Text = "Letters cannot be entered";
+                textBox2.Text = "";
+                return;
+            }
+            try
+            {
+                Quantity = Convert.ToInt32(textBox3.Text);
+            }
+            catch (OverflowException)
+            {
+                LabelProfit.Text = "Int to large!";
+                textBox3.Text = "";
+                return;
+            }
+            catch (FormatException)
+            {
+                label5.Text = "Letters cannot be entered";
+                textBox3.Text = "";
+                return;
+            }
+            int _SellingFor = Quantity * SellingFor;
+            int _BuyingFor = Quantity * BuyingFor;
+            int _Margin = SellingFor - BuyingFor;
+
+            int Profit = _SellingFor - _BuyingFor;
+            if (label1.Text != "Selling For:")
+                label1.Text = "Selling For:";
+            if (label2.Text != "Buying For:")
+                label2.Text = "Buying For:";
+            if (label5.Text != "Quantity:")
+                label5.Text = "Quantity:";
+
+            LabelProfit.Text = String.Format("{0:N0}", Profit);
+            labelInput.Text = String.Format("{0:N0}", _BuyingFor);
+            labelOutput.Text = String.Format("{0:N0}", _SellingFor);
+            labelMargin.Text = String.Format("{0:N0}", _Margin);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+                return;
+            if (string.IsNullOrEmpty(textBox2.Text))
+                return;
+            if (string.IsNullOrEmpty(textBox3.Text))
+                return;
+            string Copy = "";
+
+            Copy += "Item: " + textBox4.Text + "\n";
+
+            Copy += "Selling For: " + textBox1.Text + "\n";
+            Copy += "Buying For: " + textBox2.Text + "\n";
+            Copy += "Quantity: " + textBox3.Text + "\n";
+            Copy += "Profit: " + LabelProfit.Text + "\n";
+            Copy += "Input: " + labelInput.Text + "\n";
+            Copy += "Output: " + labelOutput.Text + "\n";
+            Clipboard.SetText(Copy);
+        }
+
+
+
+
+    }
+}
